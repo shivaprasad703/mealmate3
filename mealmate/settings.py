d74dfe3cbf31cc70354f3d1591982012d6b8d3d3
Polapally Shiva Prasad <shivaprasad.polapelly@gmail.com>
2026-06-24 03:03:33 +0530

    Use SQLite on Render

--- mealmate/settings.py
+++ mealmate/settings.py
@@ -80,12 +80,8 @@ WSGI_APPLICATION = 'mealmate.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'mealmate',
-        'USER': 'root',
-        'PASSWORD': 'root',
-        'HOST': 'localhost',
-        'PORT': '3306',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
