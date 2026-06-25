226db5fdfc20d69d2639a2286c00bd9706d0d4fd
Polapally Shiva Prasad <shivaprasad.polapelly@gmail.com>
2026-06-26 00:54:47 +0530

    Added STATIC_ROOT

--- mealmate/settings.py
+++ mealmate/settings.py
@@ -127,7 +127,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = '/static/'
-
+STATIC_ROOT = BASE_DIR / "staticfiles"
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
